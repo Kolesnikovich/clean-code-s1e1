e739181b4c854ae5ece655035eb919413db6122d
Kolesnikovich <kolesnikovich_kv_19@mf.grsu.by>
2023-04-24 16:36:19 +0300

    fix(extended 1.2): add alt for all multimedia (including created by user)

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    deleteButtonImg.alt='delete';
     label.innerText=taskString;
     label.className='task';
 
